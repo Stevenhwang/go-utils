3d8dc21b3ec521fd63f2de95b5e7b8c68d34e670
steven <steven@manwin5.com>
2018-01-23 18:28:41 +0800

    add timedate

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # go-utils
-Some useful go utils
+Some useful go utils in my projects.
+
+1. GetTodayYMD 得到以sep为分隔符的年、月、日字符串(今天)
+
+2. GetAnydayYMD 得到以sep为分隔符的年、月、日字符串(以今天为准的任何一天)
---
+++ timedate.go
@@ -0,0 +1,30 @@
+package goUtils
+
+import (
+	"strconv"
+	"strings"
+	"time"
+)
+
+// GetTodayYMD 得到以sep为分隔符的年、月、日字符串(今天)
+func GetTodayYMD(sep string) string {
+	now := time.Now()
+	todayYMD := now.Format("2006-01-02")
+
+	return strings.Replace(todayYMD, "-", sep, -1)
+}
+
+// GetAnydayYMD 得到以sep为分隔符的年、月、日字符串(以今天为准的任何一天)
+func GetAnydayYMD(sep string, any int) string {
+	if any == 0 {
+		return GetTodayYMD(sep)
+	} else {
+		now := time.Now()
+		changeStr := strconv.Itoa(any*24) + "h"
+		changeTime, _ := time.ParseDuration(changeStr)
+		anyday := now.Add(changeTime)
+		anydayYMD := anyday.Format("2006-01-02")
+
+		return strings.Replace(anydayYMD, "-", sep, -1)
+	}
+}
